485b95afeb82b9f3ee5e25124c79fd1cdcb94969
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-08-11 12:15:03 +0300

    Update urls.py

--- SACCO_SYSTEM_M_PESA/urls.py
+++ SACCO_SYSTEM_M_PESA/urls.py
@@ -12,8 +12,7 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-     2. Add a URL t
-"""
+     2. Add a
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
